45be35d51d648f2dd7f7b7b2901e833e617965c8
Mike Leahy <mikel9286@hotmail.com>
2013-12-29 14:19:53 -0500

    initial commit

---
+++ js/ContentLoader.js
@@ -0,0 +1,26 @@
+﻿(function (ContentLoader, $, undefined) {
+
+    ContentLoader.Redirect = function(route) {
+        var baseUrl = "http://localhost:34503/Index.html/";
+        window.location.href = baseUrl + route;
+    }
+
+    ContentLoader.Load = function () {
+        var currentUrl = window.location.href;
+        var index = $("div#body");
+
+        if (currentUrl.indexOf("Blog") != -1) {
+            index.load("../views/blog/Index.html");
+        }
+        else if (currentUrl.indexOf("Media") != -1) {
+            index.load("../views/services/Media.html");
+        }
+        else if (currentUrl.indexOf("Design") != -1) {
+            index.load("../views/services/Design.html");
+        }
+        else {
+            index.load("../views/home/Index.html");
+        }
+    };
+
+} (window.ContentLoader = window.ContentLoader || {}, jQuery))
---
+++ js/Main.js
@@ -0,0 +1,13 @@
+﻿(function (Main, $, undefined) {
+
+    var SetSocialBarStyle = function () {
+        if (window.pageYOffset == 0)
+            $('section.SocialIcons').addClass('SocialIconsDark');
+        else
+            $('section.SocialIcons').removeClass('SocialIconsDark');
+
+    };
+
+    $(document).ready(SetSocialBarStyle);
+    $(window).scroll(SetSocialBarStyle);
+} (window.Main = window.Main || {}, jQuery))
