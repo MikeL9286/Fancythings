b08a2ef549e59d1104d454b648059467a239e5e8
Mike Leahy <mikel9286@hotmail.com>
2013-12-29 15:49:36 -0500

    changing redirect

--- js/ContentLoader.js
+++ js/ContentLoader.js
@@ -2,9 +2,9 @@
 
     ContentLoader.Redirect = function(route) {
         // var baseUrl = "http://localhost:34503/Index.html/";
-		var baseUrl = "http://mikel9286.github.io/Fancythings/";
+		var baseUrl = "http://mikel9286.github.io/Fancythings";
 		// var baseUrl = "file:///C:/Users/Michael/Documents/Visual%20Studio%202012/Projects/FancyThings/Fancythings/";
-        window.location.href = baseUrl + route;
+        window.location.href = baseUrl + "?route=" + route;
     }
 
     ContentLoader.Load = function () {
