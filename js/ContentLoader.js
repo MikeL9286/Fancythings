59c13eb62ed969de2aca111919aad984fd58bd1f
Mike Leahy <mikel9286@hotmail.com>
2013-12-29 15:14:20 -0500

    changing file paths

--- js/ContentLoader.js
+++ js/ContentLoader.js
@@ -1,7 +1,9 @@
 ﻿(function (ContentLoader, $, undefined) {
 
     ContentLoader.Redirect = function(route) {
-        var baseUrl = "http://localhost:34503/Index.html/";
+        // var baseUrl = "http://localhost:34503/Index.html/";
+		// var baseUrl = "MikeL9286.github.io/Fancythings/";
+		var baseUrl = "file:///C:/Users/Michael/Documents/Visual%20Studio%202012/Projects/FancyThings/Fancythings/";
         window.location.href = baseUrl + route;
     }
 
@@ -10,16 +12,16 @@
         var index = $("div#body");
 
         if (currentUrl.indexOf("Blog") != -1) {
-            index.load("../views/blog/Index.html");
+            index.load("views/blog/Index.html");
         }
         else if (currentUrl.indexOf("Media") != -1) {
-            index.load("../views/services/Media.html");
+            index.load("views/services/Media.html");
         }
         else if (currentUrl.indexOf("Design") != -1) {
-            index.load("../views/services/Design.html");
+            index.load("views/services/Design.html");
         }
         else {
-            index.load("../views/home/Index.html");
+            index.load("views/home/Index.html");
         }
     };
 
