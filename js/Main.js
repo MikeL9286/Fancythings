66a7049a04f1f71a224a84cd65c218f35aeeaaca
Michael Leahy <mikel9286@hotmail.com>
2014-02-08 12:28:20 -0500

    adding reward style widget

--- js/Main.js
+++ js/Main.js
@@ -1,3 +1,18 @@
 ﻿(function (Main, $, undefined) {
     
+    Main.LoadRewardStyle = function() {
+        !function (d, s, id) {
+            var e, p = /^http:/.test(d.location) ? 'http' : 'https';
+            if (!d.getElementById(id)) {
+                e = d.createElement(s);
+                e.id = id;
+                e.src = p + '://' + 'www.rewardstyle.com/api/widgets' + '/js/ltkwidget.js';
+                d.body.appendChild(e);
+            }
+            if (typeof (window.__ltkwidget) === 'object' && document.readyState === 'complete') {
+                __ltkwidget.init();
+            }
+        }(document, 'script', 'ltkwidget-script');
+    }
+
 } (window.Main = window.Main || {}, jQuery))
