da67b345b8c8b9f2abed70ea244b18ab3eeaee2f
Michael Leahy <mikel9286@hotmail.com>
2014-02-15 18:27:30 -0500

    restyle

--- js/Main.js
+++ js/Main.js
@@ -19,8 +19,8 @@
         var footerMargin = $('footer').css('margin-top');
         footerMargin = footerMargin.substring(0, (footerMargin.length - 2));
         
-        var footerHeight = windowHeight - (socialHeight + contentHeight) - footerMargin;
-        $('.footerSize').html('wh: ' + windowHeight + ' ww: ' + $(window).width() + ' s: ' + socialHeight + ' c: ' + contentHeight + ' m: ' + footerMargin + ' f: ' + footerHeight);
+        var footerHeight = windowHeight - socialHeight - contentHeight - footerMargin;
+        $('.footerSize').html('calc: ' + windowHeight + ' - ' + socialHeight + ' - ' + contentHeight + ' - ' + footerMargin + ' = ' + footerHeight);
 
         $('footer').height(footerHeight);
     }
