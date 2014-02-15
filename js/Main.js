b8e92d728e0ed12bf1e5ff72fe5fe5f4858b6973
Michael Leahy <mikel9286@hotmail.com>
2014-02-15 12:37:40 -0500

    restyle

--- js/Main.js
+++ js/Main.js
@@ -13,19 +13,14 @@
         $('footer').removeAttr('style');
     });
 
-    Main.LoadRewardStyle = function() {
-        !function (d, s, id) {
-            var e, p = /^http:/.test(d.location) ? 'http' : 'https';
-            if (!d.getElementById(id)) {
-                e = d.createElement(s);
-                e.id = id;
-                e.src = p + '://' + 'www.rewardstyle.com/api/widgets' + '/js/ltkwidget.js';
-                d.body.appendChild(e);
-            }
-            if (typeof (window.__ltkwidget) === 'object' && document.readyState === 'complete') {
-                __ltkwidget.init();
-            }
-        }(document, 'script', 'ltkwidget-script');
+    Main.ResizeFooter = function () {
+        var socailHeight = $('.SocialIcons').height();
+        var contentHeight = $('.Content').height();
+        
+        var footerHeight = $(window).height - (socialHeight + contentHeight);
+        $('footer').height(footerHeight);
     }
+
+    window.onresize = Main.ResizeFooter;
 
 } (window.Main = window.Main || {}, jQuery))
