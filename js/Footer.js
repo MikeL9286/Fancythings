7fad5e24d9ed1e7d4c114a878a07a4bd7ee6a688
Michael Leahy <mikel9286@hotmail.com>
2014-02-20 23:48:12 -0500

    restyle

--- js/Footer.js
+++ js/Footer.js
@@ -1,18 +1,18 @@
 ﻿(function (Footer, $, undefined) {
     
     Footer.Resize = function () {
-        var windowHeight = $(window).height();
-        var socialHeight = $('.SocialIcons').height();
-        var contentHeight = $('.Content').height();
+        //var windowHeight = $(window).height();
+        //var socialHeight = $('.SocialIcons').height();
+        //var contentHeight = $('.Content').height();
 
-        var footerMargin = $('footer').css('margin-top');
-        footerMargin = footerMargin.substring(0, (footerMargin.length - 2));
+        //var footerMargin = $('footer').css('margin-top');
+        //footerMargin = footerMargin.substring(0, (footerMargin.length - 2));
 
-        var footerHeight = windowHeight - socialHeight - contentHeight - footerMargin;
-        $('.footerSize').html('calc: ' + windowHeight + ' - ' + socialHeight + ' - ' + contentHeight + ' - ' + footerMargin + ' = ' + footerHeight);
+        //var footerHeight = windowHeight - socialHeight - contentHeight - footerMargin;
+        //$('.footerSize').html('calc: ' + windowHeight + ' - ' + socialHeight + ' - ' + contentHeight + ' - ' + footerMargin + ' = ' + footerHeight);
 
-        $('footer').height(footerHeight);
-        $('footer').css('display', 'block');
+        //$('footer').height(footerHeight);
+        //$('footer').css('display', 'block');
     }
 
 }(window.Footer = window.Footer || {}, jQuery))
