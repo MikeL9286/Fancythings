35df9afe063193f417f2ba5ddca9aea11e220c9c
Michael Leahy <mikel9286@hotmail.com>
2014-02-21 23:39:11 -0500

    restyle

--- js/Footer.js
+++ js/Footer.js
@@ -1,17 +1,6 @@
 ﻿(function (Footer, $, undefined) {
     
     Footer.Resize = function () {
-        //var windowHeight = $(window).height();
-        //var socialHeight = $('.SocialIcons').height();
-        //var contentHeight = $('.Content').height();
-
-        //var footerMargin = $('footer').css('margin-top');
-        //footerMargin = footerMargin.substring(0, (footerMargin.length - 2));
-
-        //var footerHeight = windowHeight - socialHeight - contentHeight - footerMargin;
-        //$('.footerSize').html('calc: ' + windowHeight + ' - ' + socialHeight + ' - ' + contentHeight + ' - ' + footerMargin + ' = ' + footerHeight);
-
-        //$('footer').height(footerHeight);
         $('footer').css('display', 'absolute');
         Footer.ChangePosition();
     }
