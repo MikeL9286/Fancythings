4fc5a926d5e08e70ec6956e3eaf926eca5071473
Michael Leahy <mikel9286@hotmail.com>
2014-11-30 22:42:05 -0500

    hooking up services page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Fancythings::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get 'services' => 'services#index'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
