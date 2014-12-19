e49418f0e8c7d0d65b56b3bbd423deab43a6447e
Michael Leahy <mikel9286@hotmail.com>
2014-12-18 21:41:27 -0500

    updated contact me mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,7 @@
 class ContactMailer < ActionMailer::Base
-  default from: "kristin@fancythingsblog.com"
-
   def contact_email(email, message)
   	@email = email
   	@message = message
-  	mail(from: @email, to: 'kristin@fancythingsblog.com', subject: 'New Message From: me =)').deliver
+  	mail(from: @email, to: 'kristin@fancythingsblog.com', subject: 'New Inquiry!').deliver
   end
 end
