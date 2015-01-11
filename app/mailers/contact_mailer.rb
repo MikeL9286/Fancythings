1a94a5b24002e0f27f452f479402d1e454bc7094
Michael Leahy <mikel9286@hotmail.com>
2015-01-10 20:22:35 -0500

    adding name to contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,8 @@
 class ContactMailer < ActionMailer::Base
-  def contact_email(email, message)
+  def contact_email(email, name, message)
   	@email = email
+  	@name = name
   	@message = message
-  	mail(from: @email, to: 'kristin@fancythingsblog.com', subject: 'New Inquiry!').deliver
+  	mail(from: @email, to: 'kristin@fancythingsblog.com', subject: 'New Inquiry - ' + @name).deliver
   end
 end
